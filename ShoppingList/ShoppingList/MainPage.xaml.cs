c2231de68bfb0a04a77147392d18745612fb7ef2
Thomas Kison <thomas.kison@it2media.de>
2018-11-27 20:49:50 +0100

    Erste Version - kein MVVM

---
+++ ShoppingList/ShoppingList/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using ShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShoppingList
+{
+    public partial class MainPage : ContentPage
+    {
+        List<ShoppingListEntry> _entries;
+        public MainPage()
+        {
+            InitializeComponent();
+            // load model
+           _entries = new List<ShoppingListEntry> {
+                new ShoppingListEntry {
+                    Label = "Milch",
+                    Price = 0.89m,
+                    Quantity = 10,
+                    Unit = "Stck",
+                    Store = "Edeka"
+                },
+                 new ShoppingListEntry {
+                    Label = "Nuss-Nougat-Creme",
+                    Price = 2.89m,
+                    Quantity = 1,
+                    Unit = "Stck",
+                    Store = "Edeka"
+                },
+                new ShoppingListEntry {
+                    Label = "Butter",
+                    Price = 0.89m,
+                    Quantity = 2,
+                    Unit = "Stck",
+                    Store = "Edeka"
+                }
+                ,
+                new ShoppingListEntry {
+                    Label = "Hackfleisch",
+                    Price = 18,
+                    Quantity = 0.5,
+                    Unit = "kg",
+                    Store = "Rewe"
+                }
+            };
+            shoppingListView.ItemsSource = _entries;
+        }
+        
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _entries.Remove((sender as Button)?.BindingContext as ShoppingListEntry);
+            shoppingListView.ItemsSource = null;
+            shoppingListView.ItemsSource = _entries;
+        }
+    }
+}
---
+++ ShoppingList/ShoppingList/Models/ShoppingListEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    class ShoppingListEntry
+    {
+        public string Label { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public decimal Price { get; set; }
+        public string Store { get; set; }
+    }
+}
